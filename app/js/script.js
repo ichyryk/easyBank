d43e49334a5f59f59d239920a031b7adfddd5664
ichyryk <irachyryk2@gmail.com>
2021-11-01 16:21:41 +0200

    feat: finish animate mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,19 +1,27 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
+const body = document.querySelector('body');
 const overlay = document.querySelector('.overlay');
+const fadeElements = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener('click', () => {
   console.log('open hamburger');
 
   if (header.classList.contains('open')) {
     //close hamburger menu
+    body.classList.remove('noscroll');
     header.classList.remove('open');
-    overlay.classList.remove('fade-in');
-    overlay.classList.add('fade-out');
+    fadeElements.forEach(element => {
+      element.classList.add('fade-out');
+      element.classList.remove('fade-in');
+    });
   } else {
     //open hamburger menu
+    body.classList.add('noscroll');
     header.classList.add('open');
-    overlay.classList.remove('fade-out');
-    overlay.classList.add('fade-in');
+    fadeElements.forEach(element => {
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    });
   }
 });
